de5c00c683c601228ca09acd8f3a144469855629
Mohammad <mohammad.dahamshi@gmail.com>
2025-06-18 18:21:46 +0300

    Adds webpack-merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,40 +1,13 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+
+module.exports =  merge(common, { 
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+  
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: "asset/resource",
-      },
-
-
-    ],
-  },
-};
+ 
+});
